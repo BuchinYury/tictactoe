ab08c8c5bbcd3d8355d6b50d8a534be468bd5324
Buchin Iurii <IBuchin@bell-main.bellintegrator.ru>
2017-06-15 14:55:33 +0300

    delet folder

--- src/main/java/io/buchin/tictactoe/multithread/test/listenerTest/Listener.java
+++
@@ -1,8 +0,0 @@
-package io.buchin.tictactoe.multithread.test.listenerTest;
-
-/**
- * Created by IBuchin on 14.06.2017.
- */
-public interface Listener {
-    void doEvent(double number);
-}
--- src/main/java/io/buchin/tictactoe/multithread/test/observerTest/Observer.java
+++
@@ -1,8 +0,0 @@
-package io.buchin.tictactoe.multithread.test.observerTest;
-
-/**
- * Created by IBuchin on 14.06.2017.
- */
-public interface Observer {
-    void update (float temperature, float humidity, int pressure);
-}
