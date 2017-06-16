5fc9ae9d902bfeeeb1bf9df20c4f5a7f47e02d2b
Buchin Iurii <IBuchin@bell-main.bellintegrator.ru>
2017-06-16 18:54:57 +0300

    develop client-srver

--- src/main/java/io/buchin/tictactoe/multithread/client/controllers/GameController.java
+++ src/main/java/io/buchin/tictactoe/multithread/client/controllers/GameController.java
@@ -3,36 +3,44 @@ package io.buchin.tictactoe.multithread.client.controllers;
 import io.buchin.tictactoe.multithread.client.link.listeners.ControllerListener;
 import io.buchin.tictactoe.multithread.client.link.listeners.ViewListener;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by IBuchin on 13.06.2017.
  */
 public class GameController implements ViewListener {
+    Socket socket;
     ControllerListener model;
 
     public GameController(ControllerListener model, String[] args) {
         this.model = model;
+        try {
+            socket = new Socket(args[0], Integer.parseInt(args[1]));
+        } catch (IOException ignored) {}
     }
 
 
     @Override
     public void userSetUserName(String userName) {
         //TODO отправка имени пользователя на сервер и ожидание возвращения имени и роли врага и роли пользователя от сервера
-        String enemyName = "Enemy";
-        int roleUser = 1;
-        int roleEnemy = 2;
-//        int roleUser = 2;
-//        int roleEnemy = 1;
+//        String enemyName = "Enemy";
+//        int roleUser = 1;
+//        int roleEnemy = 2;
+        String[] serverResponse = sendToServerUserName(userName);
+        String enemyName = serverResponse[0];
+        int roleUser = Integer.parseInt(serverResponse[1]);
+        int roleEnemy = Integer.parseInt(serverResponse[2]);
         //TODO
 
         model.firstChangeState(userName, enemyName, roleUser, roleEnemy);
-
-
     }
 
     @Override
     public void userMove(int userMove) {
         //TODO отправка хода пользователя на сервер и ожидание ответа от сервера с ходом опонента
-        int enemyMove = 3;
+//        int enemyMove = 3;
+        int enemyMove = sendToServerUserMove(userMove);
         //TODO
 
         model.changeState(userMove, enemyMove);
@@ -41,15 +49,40 @@ public class GameController implements ViewListener {
 
     @Override
     public void userWaitEnemyMove() {
-        //TODO запррс хода опонента
-        int enemyMove = 3;
+        //TODO запрос хода опонента
+//        int enemyMove = 3;
+        int enemyMove = waitResponseFromServerWithEnemyMove();
         //TODO
         model.changeState(enemyMove);
     }
 
     @Override
     public void playAgain(int n) {
-        if (n == 1) model.playAgain();
-        if (n == 2) System.exit(0);
+        if (n == 1) {
+            sayServerWhatUserPlayAgain();
+            model.playAgain();
+        }
+        if (n == 2) {
+            try {
+                socket.close();
+            } catch (IOException ignored) {}
+            System.exit(0);
+        }
+    }
+
+    private void sendToServerUserName(String userName) {
+        try (DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+             DataInputStream ois = new DataInputStream(socket.getInputStream())) {
+
+            //TODO протокол обшения с сервером через JSON
+            oos.writeUTF(userName);
+            oos.flush();
+
+            String in = ois.readUTF();
+            //TODO
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/io/buchin/tictactoe/multithread/server/models/User.java
@@ -0,0 +1,12 @@
+package io.buchin.tictactoe.multithread.server.models;
+
+/**
+ * Created by IBuchin on 16.06.2017.
+ */
+public class User {
+    private String userName;
+    private int userRole;
+
+    private String enemyName;
+    private int enemyRole;
+}
